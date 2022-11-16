6c1126aec8536fa909fbec1ed78320c9b4489323
azertypow <azertypow@gmail.com>
2022-11-16 14:08:29 +0100

    feat: api response for subpage of media section

--- site/templates/projects.php
+++ site/templates/projects.php
@@ -9,7 +9,7 @@ require_once 'site/Utils/getImageData.php';
 
 $symposium      = $site->children()->get('mediapage')->children()->get('mediapage/symposium');
 $artistVideos   = $site->children()->get('mediapage')->children()->get('mediapage/artist-videos');
-$articles       = $site->children()->get('mediapage')->children()->get('mediapage/articles');
+$denimPop       = $site->children()->get('mediapage')->children()->get('mediapage/articles');
 $projects       = $site->children()->get('mediapage')->children()->get('mediapage/projects');
 
 //echo '<pre>';
@@ -43,13 +43,71 @@ echo  json_encode([
     })->data()
   ],
   'artist-videos' => [
-    'title'                 => $artistVideos->title()->value,
+    'title'                 => $artistVideos->title()                 ->value,
+    'description_title'     => $artistVideos->description_title()     ->value,
+    'description_subtitle'  => $artistVideos->description_subtitle()  ->value,
+    'description_author'    => $artistVideos->description_author()    ->value,
+    'text'                  => $artistVideos->text()->toBlocks()->map(function($block){
+      return $block->toHtml();
+    })->data(),
+    'children'              => $artistVideos->children()->map(function ($child) {
+      return [
+        'title'                 => $child->title()->value,
+        'subtitle'              => $child->subtitle()->value,
+        'vimeoLink'             => $child->vimeoLink()->value,
+        'author'                => $child->author()->value,
+        'category'              => $child->category()->value,
+        'description'           => $child->description()->value,
+        'article_content'       => $child->article_content()->toBlocks()->map(function($block){
+          return $block->toHtml();
+        })->data(),
+      ];
+    })->data()
   ],
-  'articles'      => [
-    'title'                 => $articles->title()->value,
+  'Denimpop'      => [
+    'title'                 => $denimPop->title()                 ->value,
+    'description_title'     => $denimPop->description_title()     ->value,
+    'description_subtitle'  => $denimPop->description_subtitle()  ->value,
+    'description_author'    => $denimPop->description_author()    ->value,
+    'text'                  => $denimPop->text()->toBlocks()->map(function($block){
+      return $block->toHtml();
+    })->data(),
+    'children'              => $denimPop->children()->map(function ($child) {
+      return [
+        'title'                 => $child->title()->value,
+        'subtitle'              => $child->subtitle()->value,
+        'vimeoLink'             => $child->vimeoLink()->value,
+        'author'                => $child->author()->value,
+        'category'              => $child->category()->value,
+        'description'           => $child->description()->value,
+        'article_content'       => $child->article_content()->toBlocks()->map(function($block){
+          return $block->toHtml();
+        })->data(),
+      ];
+    })->data()
+
   ],
   'projects'      => [
     'title'                 => $projects->title()->value,
+    'description_title'     => $projects->description_title()     ->value,
+    'description_subtitle'  => $projects->description_subtitle()  ->value,
+    'description_author'    => $projects->description_author()    ->value,
+    'text'                  => $projects->text()->toBlocks()->map(function($block){
+      return $block->toHtml();
+    })->data(),
+    'children'              => $projects->children()->map(function ($child) {
+      return [
+        'title'                 => $child->title()->value,
+        'subtitle'              => $child->subtitle()->value,
+        'vimeoLink'             => $child->vimeoLink()->value,
+        'author'                => $child->author()->value,
+        'category'              => $child->category()->value,
+        'description'           => $child->description()->value,
+        'article_content'       => $child->article_content()->toBlocks()->map(function($block){
+          return $block->toHtml();
+        })->data(),
+      ];
+    })->data()
   ],
 
 ]);
