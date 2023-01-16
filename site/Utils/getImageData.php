69d78520e1eac084df8755c3b2da22d551acf6cd
azertypow <azertypow@gmail.com>
2023-01-16 12:08:47 +0100

    feat: add image multiple size api response for object liste

--- site/Utils/getImageData.php
+++ site/Utils/getImageData.php
@@ -9,8 +9,24 @@ use Kirby\Cms\File;
 function getImageData($imageFileArray)
 {
     return array_map(function ($imageFile){
-        return [
-            'mediaUrl' => $imageFile->mediaUrl(),
-        ];
+        return getJsonEncodeImageData($imageFile);
     }, $imageFileArray);
+}
+
+
+
+function getJsonEncodeImageData(Kirby\Cms\File $file): array
+{
+    return [
+        'url'       => $file->url(),
+        'mediaUrl'  => $file->mediaUrl(),
+        'width'     => $file->width(),
+        'height'    => $file->height(),
+        'resize'    => [
+            'tiny'      => $file->resize(50, null, 10)->url(),
+            'small'     => $file->resize(500)->url(),
+            'reg'       => $file->resize(1280)->url(),
+            'large'     => $file->resize(1920)->url(),
+        ]
+    ];
 }
